9b16aeea8534150f9a2e8523282cc81aa6bf6076
Evgeny Konnov <evgen.nn.ru@rambler.ru>
2023-12-26 17:47:28 +0300

    Add New Base URL

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import Nav from "./components/Nav";
+const App = () => {
+  return (
+    <div className="container">
+      <h2>React Configure Base Path Example</h2>
+      <div className="mb-5">
+        <Link className="nav-link link-danger" to="/">
+          Home
+        </Link>
+        <Link className="nav-link link-danger" to="profile">
+          Profile
+        </Link>
+        <Link className="nav-link link-danger" to="contact">
+          Contact
+        </Link>
+      </div>
+      <div>
+        <h3>
+          <Nav />
+        </h3>
+      </div>
+    </div>
+  );
+};
+export default App;
